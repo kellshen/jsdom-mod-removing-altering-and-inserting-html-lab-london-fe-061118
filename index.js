806171372b83a92a171de7b95459615c1961e307
kellshen <kellyshen7@gmail.com>
2018-08-12 22:25:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
+document.querySelector('main').remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+newHeader.innerHTML = 'Kelly is the champion!';
